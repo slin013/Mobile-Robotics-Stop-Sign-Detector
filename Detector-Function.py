cb337188f04facb688bbd86014a4c041e0217103
slin013 <lin.ser@northeastern.edu>
2025-12-07 17:42:07 -0500

    fixing comments

--- Detector-Function.py
+++ Detector-Function.py
@@ -3,9 +3,7 @@ import numpy as np
 import os
 import shutil
 
-# =========================================================
-# PREP OUTPUT FOLDER (fresh each run)
-# =========================================================
+# creates output images folder/clears old one
 output_root = "output-images"
 
 if os.path.exists(output_root):
@@ -13,44 +11,41 @@ if os.path.exists(output_root):
 
 os.makedirs(output_root)
 
-
-# =========================================================
-# LOAD REFERENCE IMAGE + ORB FEATURES
-# =========================================================
+# loads reference image
 reference_path = "reference.jpg"
 ref_img = cv2.imread(reference_path)
 max_dim = 500
 h, w = ref_img.shape[:2]
+
+# resizes output image to be smaller & adds gaussian blur
 scale = max_dim / max(h, w)
 if scale < 1:
     ref_img = cv2.resize(ref_img, (int(w * scale), int(h * scale)))
 ref_img = cv2.GaussianBlur(ref_img, (3,3), 0)
-
 
 if ref_img is None:
     raise ValueError("Error: cannot load reference image. Check the file path!")
 
 ref_gray = cv2.cvtColor(ref_img, cv2.COLOR_BGR2GRAY)
 
+# loads ORB features
 orb = cv2.ORB_create(nfeatures=2000)
 kp1, des1 = orb.detectAndCompute(ref_gray, None)
 
-
-# =========================================================
-# STOP SIGN DETECTION FUNCTION
-# =========================================================
+# stop sign detection function takes in a test image & returns output, true/false if it was detected, & number of inliers
 def detect_stop_sign(test_img):
+
+    # resizes test image & adds gaussian noise just like reference image
     max_dim = 500
     h, w = test_img.shape[:2]
     scale = max_dim / max(h, w)
     if scale < 1:
         test_img = cv2.resize(test_img, (int(w * scale), int(h * scale)))
-
     test_img = cv2.GaussianBlur(test_img, (3, 3), 0)
 
     test_gray = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
-    # Extract ORB features
+    # extracts ORB features from test image
     kp2, des2 = orb.detectAndCompute(test_gray, None)
     if des2 is None:
         output = test_img.copy()
@@ -59,27 +54,27 @@ def detect_stop_sign(test_img):
 
         return output, False, 0
 
-    # Match descriptors
+    # matches descriptors using brute force matching function and comparing hamming distances
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = bf.match(des1, des2)
     matches = sorted(matches, key=lambda x: x.distance)
     good_matches = matches[:40]
 
-    # Extract point coordinates
+    # extracts point coordinates
     ref_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
     test_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
 
-    # Compute homography
+    # computes homography & inliers
     H, mask = cv2.findHomography(ref_pts, test_pts, cv2.RANSAC, 5.0)
     inliers = mask.ravel().tolist().count(1) if mask is not None else 0
     print("Inliers:", inliers)
 
-    # Detection threshold
+    # sets inlier detection threshold
     if H is None or inliers < 9:
         output = test_img.copy()
         cv2.putText(output, "NOT DETECTED", (30, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 3)
-        cv2.putText(output, "INLIERS:" + str(inliers), (30, 90),
+        cv2.putText(output, "Matches:" + str(inliers), (30, 90),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 3)
         return output, False, inliers
 
@@ -88,20 +83,19 @@ def detect_stop_sign(test_img):
     corners = np.float32([[0,0],[w,0],[w,h],[0,h]]).reshape(-1,1,2)
     projected = cv2.perspectiveTransform(corners, H)
 
+    # creates a copy of the image and returns it as output
     output = test_img.copy()
     cv2.polylines(output, [np.int32(projected)], True, (191,64,191), 3)
     cv2.putText(output, "DETECTED", (30, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 3)
-    cv2.putText(output, "INLIERS:" + str(inliers), (30, 90),
+    cv2.putText(output, "Matches:" + str(inliers), (30, 90),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 3)
 
     return output, True, inliers
 
 
-# =========================================================
-# PROCESS FOLDERS + COMPUTE ACCURACY
-# =========================================================
 
+# Processes through folders & computes accuracy
 folders = {
     "stop-close": "stop-close",
     "stop-angle": "stop-angle",
@@ -112,18 +106,19 @@ folders = {
 
 results = {}
 
+# Loops through each folder and all images within the folder
 for folder_name, folder_path in folders.items():
     print("\nProcessing folder:", folder_name)
 
     total_images = 0
     correct = 0
 
-    # Create subfolder inside output-images
+    # creates subfolder inside output-images
     save_folder = os.path.join(output_root, folder_name)
     os.makedirs(save_folder, exist_ok=True)
 
     for filename in os.listdir(folder_path):
-        if filename.lower().endswith((".jpg", ".jpeg", ".png",".webp", ".heic")):
+        if filename.lower().endswith((".jpg", ".jpeg")):
 
             total_images += 1
             img_path = os.path.join(folder_path, filename)
@@ -163,7 +158,7 @@ for folder_name, folder_path in folders.items():
 # =========================================================
 # FINAL SUMMARY
 # =========================================================
-print("\n====== FINAL ACCURACY SUMMARY ======")
+print("\n FINAL ACCURACY SUMMARY")
 for folder_name, acc in results.items():
     print(f"{folder_name}: {acc:.2f}%")
 
